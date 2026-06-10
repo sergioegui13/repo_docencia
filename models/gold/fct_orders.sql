a9e9a6927ac5695a5b7a79612420f39889577573
Eduardo Ransanz Manzano <eduardo.ransanz@civica-soft.com>
2026-06-10 17:16:46 +0000

    B4

--- models/gold/fct_orders.sql
+++ models/gold/fct_orders.sql
@@ -1,9 +1,16 @@
 -- Gold: tabla de hechos de pedidos, lista para consumo de BI.
 -- En el ejercicio B4 la convertirás en incremental.
+
+{{config(
+    materialized='incremental',
+    unique_key='order_id',
+    incremental_strategy='merge',
+    on_schema_change='append_new_column'
+)}}
+
 select
-    order_id,
-    customer_id,
-    order_date,
-    status,
-    amount
+    *
 from {{ ref('slv_orders') }}
+{% if is_incremental() %}
+    where order_date > (select max(order_date) from {{ this }})
+{% endif %}
